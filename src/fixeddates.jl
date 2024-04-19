57ef6f1bebab794122a388d4c2caea9936c3f0f3
Neel Smith <dnsmith.neel@gmail.com>
2024-04-19 03:34:58 -0400

    Dates for main feasts and holy days

--- src/fixeddates.jl
+++ src/fixeddates.jl
@@ -27,9 +27,44 @@ fixed_dates = Dict(
     # Major feasts that take precedence over Sundays:
     FEAST_HOLY_NAME => FixedDate(1,1),
     FEAST_PRESENTATION => FixedDate(2,2),
-    FEAST_TRANSFIGURATION => FixedDate(8,6)
+    FEAST_TRANSFIGURATION => FixedDate(8,6),
+    FEAST_CONFESSION_OF_PETER => FixedDate(1,18),
+    FEAST_CONVERSION_OF_PAUL => FixedDate(1,25),
+    FEAST_SAINT_MATTHIAS => FixedDate(2,24),
+    FEAST_SAINT_JOSEPH => FixedDate(3,19),
+    FEAST_ANNUNCIATION => FixedDate(3,25),
+    FEAST_SAINT_MARK => FixedDate(4,25),
+    FEAST_SAINTS_PHILIP_AND_JAMES => FixedDate(5,1),
+    FEAST_VISITATION => FixedDate(5,31),
+    FEAST_SAINT_BARNABAS => FixedDate(6,11),
+    FEAST_SAINT_JOHN_BAPTIST => FixedDate(6,24),
+    FEAST_CONFESSION_OF_PETER => FixedDate(6,29),
+    FEAST_CONVERSION_OF_PAUL => FixedDate(6,29),
 
+    FEAST_INDEPENDENCE_DAY => FixedDate(7,4,)
+    FEAST_MARY_MAGDALENE => FixedDate(7,22),
+    FEAST_SAINT_JAMES_APOSTLE => FixedDate(7,25),
+
+    FEAST_MARY_THE_VIRGIN => FixedDate(8,15),
+    FEAST_SAINT_BARTHOLOMEW => FixedDate(8,24),
+    FEAST_HOLY_CROSS => FixedDate(9,14),
+    FEAST_SAINT_MATTHEW => FixedDate(9,21),
+    FEAST_MICHAEL_AND_ALL_ANGELS => FixedDate(9,29),
+
+    FEAST_SAINT_LUKE => FixedDate(10,18),
+    FEAST_SAINT_JAMES_JERUSALEM => FixedDate(10,23),
+    FEAST_SAINTS_SIMON_AND_JUDE => FixedDate(10,28),
+
+    FEAST_SAINT_ANDREW => FixedDate(11,30),
+
+    FEAST_SAINT_THOMAS => FixedDate(12,21),
+    FEAST_SAINT_STEPHEN => FixedDate(12,26),
+    FEAST_SAINT_JOHN => FixedDate(12,27)
+    FEAST_HOLY_INNOCENTS => FixedDate(12,28)
 )
+#=
+const FEAST_THANKSGIVING_DAY = 24
+=#
 
 
 
