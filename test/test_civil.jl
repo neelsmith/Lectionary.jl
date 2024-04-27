acb71479d9dd8c046c3561c16cc67322ac8d035b
Neel Smith <dnsmith.neel@gmail.com>
2024-04-27 08:48:02 -0400

    text formatting

--- test/test_civil.jl
+++ test/test_civil.jl
@@ -2,10 +2,11 @@
 @testset "Test computation of dates in civil calendar in regular year" begin
     # Calendar year 2023 was a normal year, so test dates in liturgical year
     # 2022-2023.
-    # 
-    # Expected values taken from https://www.lectionarypage.net/CalndrsIndexes/Calendar2023.html:
 
     lityr = LiturgicalYear(2022)
+    
+    # Expected values taken from https://www.lectionarypage.net/CalndrsIndexes/Calendar2023.html:
+    
     @test civildate(easter_sunday(lityr))  == Date(2023,4,9)
     @test ash_wednesday_date(lityr) == Date(2023,2,22)
     @test ascension(lityr) == Date(2023, 5, 18)
@@ -16,10 +17,11 @@ end
 @testset "Test computation of dates in civil calendar in leap year" begin
     # Calendar year 2024 was a leap year, test dates in liturgical year
     # 2023-2024.
-    #
-    # Expected values taken from https://www.lectionarypage.net/CalndrsIndexes/Calendar2024.html:
     
-     lityr = LiturgicalYear(2023)
+    lityr = LiturgicalYear(2023)
+    
+    # Expected values taken from https://www.lectionarypage.net/CalndrsIndexes/Calendar2024.html:
+     
      @test civildate(easter_sunday(lityr))  == Date(2024,3,31)
      @test ash_wednesday_date(lityr) == Date(2024,2,14)
      @test ascension(lityr) == Date(2024, 5, 9)
