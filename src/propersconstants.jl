b8d88f1e0385209e1893fa27c6814accd24e530f
Neel Smith <dnsmith.neel@gmail.com>
2024-04-29 11:19:13 -0400

    Raw data for Year B Propers

--- src/propersconstants.jl
+++ src/propersconstants.jl
@@ -112,112 +112,173 @@ propersA = Dict(
 
 propersB = Dict(
     3 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "Hosea 2.14-2.20","2_Corinthians 3.1-3.6","Mark 2.13-2.22","Psalm 103.1-103.13, Psalm 103.22"),
+        B = Reading("Hosea 2.14-2.20","2_Corinthians 3.1-3.6","Mark 2.13-2.22","")
     ),
     4 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Samuel 3:1-10 [11-20] Psalm 139:1-6, 13-18
+        2 Corinthians 4:5-12 Mark 2:23—3:6"),
+        B = Reading("","","","Deuteronomy 5:12-15 Psalm 81:1-10
+        2 Corinthians 4:5-12 Mark 2:23—3:6")
     ),
     5 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Samuel 8:4-11 [12-15] 16-20 [11:14-15] Psalm 138
+        2 Corinthians 4:13—5:1
+        Mark 3:20-35"),
+        B = Reading("","","","Genesis 3:8-15
+        Psalm 130
+        2 Corinthians 4:13—5:1 Mark 3:20-35")
     ),
     6 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Samuel 15:34—16:13
+        Psalm 20
+        2 Corinthians 5:6-10 [11-13] 14-17 Mark 4:26-34"),
+        B = Reading("","","","Ezekiel 17:22-24
+        Psalm 92:1-4, 12-15
+        2 Corinthians 5:6-10 [11-13] 14-17 Mark 4:26-34")
     ),
     7 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Samuel 17:[1a, 4-11, 19-23] 32-49 or 1 Samuel 17:57—18:5, 10-16 Psalm 9:9-20 or Psalm 133
+        2 Corinthians 6:1-13
+        Mark 4:35-41"),
+        B = Reading("","","","Job 38:1-11
+        Psalm 107:1-3, 23-32 2 Corinthians 6:1-13 Mark 4:35-41")
     ),
     8 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 1:1, 17-27 Psalm 130 2 Corinthians 8:7-15 Mark 5.21-5.23"),
+        B = Reading("","","","Wisdom of Solomon 1:13-15; 2:23-24 or Lamentations 3:22-33
+        Psalm 30
+        2 Corinthians 8:7-15
+        Mark 5:21-43")
     ),
     9 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 5:1-5, 9-10 Psalm 48
+        2 Corinthians 12:2-10 Mark 6:1-13"),
+        B = Reading("","","","Amos 7:7-15 Psalm 85:8-13 Ephesians 1:3-14 Mark 6:14-29")
     ),
     10 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","",""," Samuel 6:1-5, 12b-19 Psalm 24
+        Ephesians 1:3-14
+        Mark 6:14-29"),
+        B = Reading("","","","Amos 7:7-15 Psalm 85:8-13 Ephesians 1:3-14 Mark 6:14-29")
     ),
     11 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 7:1-14a Psalm 89:20-37 Ephesians 2:11-22 Mark 6:30-34, 53-56"),
+        B = Reading("","","","Jeremiah 23:1-6 Psalm 23
+        Ephesians 2:11-22 Mark 6:30-34, 53-56")
     ),
     12 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 11:1-15 Psalm 14 Ephesians 3:14-21 John 6:1-21"),
+        B = Reading("","","","2 Kings 4:42-44 Psalm 145:10-18 Ephesians 3:14-21 John 6:1-21")
     ),
     13 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 11:26—12:13a Psalm 51:1-12 Ephesians 4:1-16
+        John 6:24-35"),
+        B = Reading("","","","Exodus 16:2-4, 9-15 Psalm 78:23-29 Ephesians 4:1-16 John 6:24-35")
     ),
     14 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 18:5-9, 15, 31-33 Psalm 130
+        Ephesians 4:25—5:2
+        John 6:35, 41-51"),
+        B = Reading("","","","1 Kings 19:4-8 Psalm 34:1-8 Ephesians 4:25—5:2 John 6:35, 41-51")
     ),
-    15 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+    15 => (
+        A = Reading("","","","1 Kings 2:10-12; 3:3-14 Psalm 111
+        Ephesians 5:15-20
+        John 6:51-58"),
+        B = Reading("","","","Proverbs 9:1-6 Psalm 34:9-14 Ephesians 5:15-20 John 6:51-58")
     ),
     16 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Kings 8:[1, 6, 10-11] 22-30, 41-43 Psalm 84
+        Ephesians 6:10-20
+        John 6:56-69"),
+
+        B = Reading(  
+        "","","","Joshua 24:1-2a, 14-18 Psalm 34:15-22 Ephesians 6:10-20 John 6:56-69"),
     ),
+
     17 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Song of Solomon 2:8-13 Psalm 45:1-2, 6-9
+James 1:17-27
+Mark 7:1-8, 14-15, 21-23"),
+        B = Reading("","","","Deuteronomy 4:1-2, 6-9 Psalm 15
+        James 1:17-27
+        Mark 7:1-8, 14-15, 21-23")
     ),
     18 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Proverbs 22:1-2, 8-9, 22-23 Psalm 125
+        James 2:1-10 [11-13] 14-17 Mark 7:24-37"),
+        B = Reading("","","","Isaiah 35:4-7a
+        Psalm 146
+        James 2:1-10 [11-13] 14-17 Mark 7:24-37")
     ),
     19 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Proverbs 1:20-33
+        Psalm 19
+        or Wisdom of Solomon 7:26—8:1 James 3:1-12
+        Mark 8:27-38"),
+        B = Reading("","","","Isaiah 50:4-9a Psalm 116:1-9
+        James 3:1-12 Mark 8:27-38")
     ),
     20 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Proverbs 31:10-31
+        Psalm 1
+        James 3:13—4:3, 7-8a Mark 9:30-37"),
+        B = Reading("","","","Wisdom of Solomon 1:16—2:1, 12-22 or Jeremiah 11:18-20
+        Psalm 54
+        James 3:13—4:3, 7-8a
+        Mark 9:30-37")
     ),
     21 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Esther 7:1-6, 9-10; 9:20-22 Psalm 124
+        James 5:13-20
+        Mark 9:38-50"),
+        B = Reading("","","","Numbers 11:4-6, 10-16, 24-29 Psalm 19:7-14
+        James 5:13-20
+        Mark 9:38-50")
     ),
     22 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Job 1:1; 2:1-10
+        Psalm 26
+        Hebrews 1:1-4; 2:5-12 Mark 10:2-16"),
+        B = Reading("","","","Genesis 2:18-24 Psalm 8
+        Hebrews 1:1-4; 2:5-12 Mark 10:2-16")
     ),
     23 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Job 23:1-9, 16-17 Psalm 22:1-15 Hebrews 4:12-16 Mark 10:17-31"),
+        B = Reading("","","","Amos 5:6-7, 10-15 Psalm 90:12-17 Hebrews 4:12-16 Mark 10:17-31")
     ),
     24 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Job 38:1-7 [34-41] Psalm 104:1-9, 24, 35b Hebrews 5:1-10
+        Mark 10:35-45"),
+        B = Reading("","","","Isaiah 53:4-12 Psalm 91:9-16 Hebrews 5:1-10 Mark 10:35-45")
     ),
     25 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Job 42:1-6, 10-17 Psalm 34:1-8 [19-22] Hebrews 7:23-28 Mark 10:46-52"),
+        B = Reading("","","","Jeremiah 31:7-9 Psalm 126 Hebrews 7:23-28 Mark 10:46-52")
     ),
     26 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Ruth 1:1-18 Psalm 146 Hebrews 9:11-14 Mark 12:28-34"),
+        B = Reading("","","","Deuteronomy 6:1-9 Psalm 119:1-8 Hebrews 9:11-14 Mark 12:28-34")
     ),
     27 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","Ruth 3:1-5; 4:13-17 Psalm 127 Hebrews 9:24-28 Mark 12:38-44"),
+        B = Reading("","",""," Kings 17:8-16 Psalm 146 Hebrews 9:24-28 Mark 12:38-44")
     ),
     28 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","1 Samuel 1:4-20
+        1 Samuel 2:1-10
+        Hebrews 10:11-14 [15-18] 19-25 Mark 13:1-8"),
+        B = Reading("","","","Daniel 12:1-3
+        Psalm 16
+        Hebrews 10:11-14 [15-18] 19-25 Mark 13:1-8")
     ),
     29 => (A = Reading(  
-        "","","",""),
-        B = Reading("","","","")
+        "","","","2 Samuel 23:1-7
+        Psalm 132:1-12 [13-18] Revelation 1:4b-8
+        John 18:33-37"),
+        B = Reading("","","","Daniel 7:9-10, 13-14 Psalm 93
+        Revelation 1:4b-8 John 18:33-37")
     ),
 )
 
